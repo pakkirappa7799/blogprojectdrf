57697766655b1f8521876002c97eded7ecfd5c18
pakkirappa7799 <105942253+pakkirappa7799@users.noreply.github.com>
2023-11-27 00:27:49 +0530

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-ssh root@44.202.152.45<<EOF
-  cd /Pakkirappa/blogprojectdrf 
+ssh root@44.202.152.45 <<EOF
+  cd /root/Pakkirappa/blogprojectdrf 
   git pull 
   source venv/bin/activate
-  ./manage.py migrate
+  python manage.py makemigrations
+  python manage.py migrate
   sudo systemctl restart nginx
   sudo service gunicorn restart
   sudo service nginx restart
